bd1ec84a39e7dae656b2e713138cb02810491444
Jonathon-Coe-Bassett <j.c.bassett90@gmail.com>
2016-09-22 08:49:45 -0600

    Added main

--- IQPuzzle/src/runGame.java
+++ IQPuzzle/src/runGame.java
@@ -4,7 +4,7 @@ public class runGame
 
 		public static void main(String[] args)
 			{
-				//Steve, you had tod me you didn't have a main method. Here it is. I didn't do too much to screw up your code.
+				//Steve, you had tod me you didn't have a main method. Here it is. I didn't do too much to break your code.
 
 			}
 
